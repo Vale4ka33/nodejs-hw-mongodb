fd6dbe36df07b4be2e7758214336d50073174fb3
Valentine Ukho <petrova.hfhfhf@gmail.com>
2024-08-04 17:14:09 -1000

    try to fix server error status

--- src/server.js
+++ src/server.js
@@ -20,44 +20,31 @@ const setupServer = () => {
   );
 
   app.get('/contacts', async (req, res) => {
-    try {
-      const contacts = await getAllContacts();
-      res.status(200).json({
-        status: 200,
-        message: 'Successfully found contacts!',
-        data: contacts,
-      });
-    } catch (error) {
-      res.status(500).json({
-        status: 500,
-        message: error.message,
-      });
-    }
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
   });
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    try {
-      const { contactId } = req.params;
-      const contact = await getContactById(contactId);
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const contactId = req.params.contactId;
+    const contact = await getContactById(contactId);
 
-      if (contact) {
-        res.status(200).json({
-          status: 200,
-          message: `Successfully found contact with id ${contactId}!`,
-          data: contact,
-        });
-      } else {
-        res.status(404).json({
-          status: 404,
-          message: 'Contact not found',
-        });
-      }
-    } catch (error) {
-      res.status(500).json({
-        status: 500,
-        message: error.message,
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
       });
+      return;
     }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
   });
 
   app.use('*', (req, res) => {
