e48bab750ac450f3d405dc6dfa7a2726c63da226
Valentine Ukho <petrova.hfhfhf@gmail.com>
2024-09-08 15:10:49 -1000

    change some description

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -5,9 +5,10 @@ import { SWAGGER_PATH } from '../constants/index.js';
 
 export const swaggerDocs = () => {
   try {
-    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
+    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH, 'utf8'));
     return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
   } catch (error) {
+    console.error('Error loading Swagger documentation:', error);
     return (req, res, next) =>
       next(createHttpError(500, "Can't load swagger docs"));
   }
